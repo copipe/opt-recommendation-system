79cce57cb9083fbb3ea0a52fa82b2da43400271c
copipe <taka.0122.kuro@gmail.com>
2022-10-14 09:04:10 +0000

    completed operation check of ndcg_score

--- src/metrics.py
+++ src/metrics.py
@@ -1,23 +1,17 @@
 import numpy as np
 
 
-def dcg_score(y_true, y_score, k=10, gains="exponential"):
-    order = np.argsort(y_score)[::-1]
-    y_true = np.take(y_true, order[:k])
-
-    if gains == "exponential":
-        gains = 2**y_true - 1
-    elif gains == "linear":
-        gains = y_true
-    else:
-        raise ValueError("Invalid gains option.")
-
-    # highest rank is 1 so +2 instead of +1
-    discounts = np.log2(np.arange(len(y_true)) + 2)
+def dcg_score(scores: np.ndarray) -> np.float64:
+    gains = np.array([2**s - 1 for s in scores])
+    discounts = np.log2(np.arange(len(scores)) + 2)
     return np.sum(gains / discounts)
 
 
-def ndcg_score(y_true, y_score, k=10, gains="exponential"):
-    best = dcg_score(y_true, y_true, k, gains)
-    actual = dcg_score(y_true, y_score, k, gains)
-    return actual / best
+def ndcg_score(y_true: np.ndarray, y_pred: np.ndarray, k: int = 22) -> np.float64:
+    y_true = y_true[:k]
+    y_pred = y_pred[:k]
+    y_true = np.pad(y_true, (0, k - len(y_true)))
+    y_pred = np.pad(y_pred, (0, k - len(y_pred)))
+    ideal_ndcg = dcg_score(y_true)
+    actual_ndcg = dcg_score(y_pred)
+    return actual_ndcg / ideal_ndcg
