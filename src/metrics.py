e185a98a6aded182934247911ec1ac5e6978b2ca
copipe <taka.0122.kuro@gmail.com>
2022-10-14 01:20:38 +0000

    add ndcg

---
+++ src/metrics.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def dcg_score(y_true, y_score, k=10, gains="exponential"):
+    order = np.argsort(y_score)[::-1]
+    y_true = np.take(y_true, order[:k])
+
+    if gains == "exponential":
+        gains = 2**y_true - 1
+    elif gains == "linear":
+        gains = y_true
+    else:
+        raise ValueError("Invalid gains option.")
+
+    # highest rank is 1 so +2 instead of +1
+    discounts = np.log2(np.arange(len(y_true)) + 2)
+    return np.sum(gains / discounts)
+
+
+def ndcg_score(y_true, y_score, k=10, gains="exponential"):
+    best = dcg_score(y_true, y_true, k, gains)
+    actual = dcg_score(y_true, y_score, k, gains)
+    return actual / best
