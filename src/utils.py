06b8c9d886853a438e4ca0ca350d70c90f68fcc5
copipe <taka.0122.kuro@gmail.com>
2022-10-15 12:47:15 +0000

    add order_immutable_deduplication function

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def order_immutable_deduplication(items: List[str]) -> List[str]:
+    """
+    Remove duplicates while preserving array order.
+    (Assumed to be applied to the candidate item list)
+
+    Args:
+        items (List[str]): Item list.
+
+    Returns:
+        _type_: Deduped item list.
+    """
+    return sorted(set(items), key=items.index)
