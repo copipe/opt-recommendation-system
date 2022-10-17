6d60b297ab21c50ddb3659152a152c8e6ecefd6a
copipe <taka.0122.kuro@gmail.com>
2022-10-17 04:39:19 +0000

    refactor for ConcatRetriever

--- src/retriever.py
+++ src/retriever.py
@@ -11,69 +11,42 @@ import pandas as pd
 from tqdm import tqdm
 
 from src.metrics import ndcg_score, precision_score, recall_score
-from src.utils import flatten_2d_list, order_immutable_deduplication
-
-
-def get_data_period(
-    date_th: str, train_period: int, eval_period: int
-) -> Tuple[pd.Timestamp, pd.Timestamp, pd.Timestamp, pd.Timestamp]:
-    """Calculate the first and last days of the train period and evaluation period.
-
-    Args:
-        date_th (str): Boundary between train period and evaluation period.
-        train_period (int): Number of days of train period.
-        eval_period (int, optional): Number of days of evaluation period. Defaults to 7.
-
-    Returns:
-        Tuple[pd.Timestamp, pd.Timestamp, pd.Timestamp, pd.Timestamp]: First and last day of study period and evaluation period.
-    """
-    train_start = pd.to_datetime(date_th) - pd.Timedelta(train_period, "days")
-    train_end = pd.to_datetime(date_th)
-    eval_start = pd.to_datetime(date_th)
-    eval_end = pd.to_datetime(date_th) + pd.Timedelta(eval_period, "days")
-    return train_start, train_end, eval_start, eval_end
-
-
-def period_extraction(
-    df: cudf.DataFrame, start_date: pd.Timestamp, end_date: pd.Timestamp
-) -> cudf.DataFrame:
-    """Extract data for specific period.
-
-    Args:
-        df (cudf.DataFrame): Preprocessed data.
-        start_date (pd.Timestamp): First day of extraction period.
-        end_date (pd.Timestamp): Last day of extraction period.
-
-    Returns:
-        cudf.DataFrame: Data after extraction.
-    """
-    df = df[
-        (start_date < df["time_stamp"]) & (df["time_stamp"] <= end_date)
-    ].reset_index(drop=True)
-    return df
+from src.utils import flatten_2d_list, order_immutable_deduplication, period_extraction
 
 
 class Retriever(metaclass=ABCMeta):
     """Abstract class of Retriever (recommendation candidate selection class)
 
     Attributes:
+        train_start_date (pd.Timestamp): First day of train period.
+        train_end_date (pd.Timestamp): Last day of train period.
+        eval_start_date (pd.Timestamp): First day of evaluation period.
+        eval_end_date (pd.Timestamp): Last day of evaluation period.
         top_n (int): Get top n popular items.
         candidate_items (Dict[str, List[str]]): Item list of recommendation candidates searched for by user.
         category_types (List[str]) : List of category types. (A:人材, B:旅行, C:不動産, D:アパレル)
+        name (str): Class name.
     """
 
     def __init__(
         self,
+        train_start_date: pd.Timestamp,
+        train_end_date: pd.Timestamp,
+        eval_start_date: pd.Timestamp,
+        eval_end_date: pd.Timestamp,
         top_n: int = 10,
     ) -> None:
+        self.train_start_date = train_start_date
+        self.train_end_date = train_end_date
+        self.eval_start_date = eval_start_date
+        self.eval_end_date = eval_end_date
         self.top_n = top_n
         self.candidate_items: Dict[str, List[str]] = {}
         self.category_types: List[str] = ["A", "B", "C", "D"]
+        self.name: str = self.__class__.__name__
 
     @abstractclassmethod
-    def fit(
-        self, df: cudf.DataFrame, start_date: pd.Timestamp, end_date: pd.Timestamp
-    ) -> None:
+    def fit(self, df: cudf.DataFrame) -> None:
         pass
 
     @abstractclassmethod
@@ -83,8 +56,6 @@ class Retriever(metaclass=ABCMeta):
     def evaluate(
         self,
         df: cudf.DataFrame,
-        start_date: pd.Timestamp,
-        end_date: pd.Timestamp,
         k=22,
         verbose: bool = True,
     ) -> Tuple[float, float, float]:
@@ -92,8 +63,6 @@ class Retriever(metaclass=ABCMeta):
 
         Args:
             df (cudf.DataFrame): Preprocessed data.
-            start_date (pd.Timestamp): First day of evaluation period.
-            end_date (pd.Timestamp): Last day of evaluation period.
             k (int): When calculating ndcg, up to the top k are evaluated. Defaults to 22.
             verbose (bool): Verbosity mode.
 
@@ -101,7 +70,7 @@ class Retriever(metaclass=ABCMeta):
             Tuple[float, float, float]: (Theoretical max NDCG@K, Recall, Precision)
         """
         # Extract only the data for the evaluation period
-        df = period_extraction(df, start_date, end_date)
+        df = period_extraction(df, self.eval_start_date, self.eval_end_date)
 
         # Remove other and (cv=1 and ad !=1). (cv:3, click:2, view:1, other:0)
         df = df[df["event_type"] > 0]
@@ -125,6 +94,7 @@ class Retriever(metaclass=ABCMeta):
 
         # Calculate 3 evaluation indexes (Theoritical max NDCG@K, Recall, Precision)
         n_users = 0
+        sum_n_items = 0
         sum_max_ndcg = 0
         sum_recall = 0
         sum_precision = 0
@@ -133,16 +103,18 @@ class Retriever(metaclass=ABCMeta):
             pred_scores = true_scores[np.isin(true_items, pred_items)]
 
             n_users += 1
+            sum_n_items += len(pred_items)
             sum_max_ndcg += ndcg_score(true_scores, pred_scores, k)
             sum_recall += recall_score(true_items, pred_items)
             sum_precision += precision_score(true_items, pred_items)
+        n_items = sum_n_items / n_users
         max_ndcg = sum_max_ndcg / n_users
         recall = sum_recall / n_users
         precision = sum_precision / n_users
         if verbose:
             msg = (
-                f"[{self.__class__.__name__}] "
-                f"n={n_users:,}, "
+                f"[{self.name}] "
+                f"n={n_users:,}, n_items={n_items:.1f} "
                 f"max_ndcg={max_ndcg:.4f}, recall={recall:.4f}, precision={precision:.4f}"
             )
             print(msg)
@@ -165,13 +137,11 @@ class Retriever(metaclass=ABCMeta):
 class PopularItem(Retriever):
     """Select recently popular items as candidate items."""
 
-    def fit(
-        self, df: cudf.DataFrame, start_date: pd.Timestamp, end_date: pd.Timestamp
-    ) -> None:
+    def fit(self, df: cudf.DataFrame) -> None:
         """Aggregate the recent popular items."""
 
         # Extract only the data for the train period.
-        df = period_extraction(df, start_date, end_date)
+        df = period_extraction(df, self.train_start_date, self.train_end_date)
 
         # Get top N popular items (items with lots of cv, click, view) for each category.
         df = df[df["event_type"] > 0]
@@ -192,13 +162,11 @@ class PopularItem(Retriever):
 class FavoriteItem(Retriever):
     """Select recent favorite items as candidate items."""
 
-    def fit(
-        self, df: cudf.DataFrame, start_date: pd.Timestamp, end_date: pd.Timestamp
-    ) -> None:
+    def fit(self, df: cudf.DataFrame) -> None:
         """Aggregate the recent favorite items."""
 
         # Extract only the data for the train period.
-        df = period_extraction(df, start_date, end_date)
+        df = period_extraction(df, self.train_start_date, self.train_end_date)
 
         # Get top N recent favorite items (items with cv, click, view).
         df = df[df["event_type"] > 0]
@@ -220,20 +188,35 @@ class FavoriteItem(Retriever):
 
 
 class CoOccurrenceItem(Retriever):
+    def __init__(
+        self,
+        output_path: Path,
+        train_start_date: pd.Timestamp,
+        train_end_date: pd.Timestamp,
+        eval_start_date: pd.Timestamp,
+        eval_end_date: pd.Timestamp,
+        top_n: int = 10,
+    ) -> None:
+        super().__init__(
+            train_start_date, train_end_date, eval_start_date, eval_end_date, top_n
+        )
+        self.output_path = output_path
+
     def fit(
         self,
         df: cudf.DataFrame,
-        start_date: pd.Timestamp,
-        end_date: pd.Timestamp,
-        output_path: Path = None,
     ) -> None:
 
         # Extract only the data for the train period.
-        df = period_extraction(df, start_date, end_date)
+        df = period_extraction(
+            df,
+            self.train_start_date,
+            self.train_end_date,
+        )
         # Remove other. (cv:3, click:2, view:1, other:0)
         df = df[df["event_type"] > 0]
 
-        if not output_path.exists():
+        if not self.output_path.exists():
             # Extract top_n items that co-occur with each item.
             self.co_occur_items: Dict[str, List[str]] = {}
             items = df["product_id"].unique().to_pandas()
@@ -243,11 +226,11 @@ class CoOccurrenceItem(Retriever):
                 vc = df_["product_id"].value_counts()[: self.top_n]
                 self.co_occur_items[item] = vc.index.to_arrow().tolist()
 
-            output_path.parent.mkdir(parents=True, exist_ok=True)
-            with open(output_path, "wb") as f:
+            self.output_path.parent.mkdir(parents=True, exist_ok=True)
+            with open(self.output_path, "wb") as f:
                 pickle.dump(self.co_occur_items, f)
         else:
-            with open(output_path, "rb") as f:
+            with open(self.output_path, "rb") as f:
                 self.co_occur_items = pickle.load(f)
 
         df = df.sort_values(["user_id", "time_stamp"], ascending=False)
@@ -269,17 +252,21 @@ class ConcatRetriever(Retriever):
     def __init__(
         self,
         retrievers: List[Retriever],
+        train_start_date: pd.Timestamp,
+        train_end_date: pd.Timestamp,
+        eval_start_date: pd.Timestamp,
+        eval_end_date: pd.Timestamp,
         top_n: int = 10,
     ) -> None:
-        super().__init__(top_n)
+        super().__init__(
+            train_start_date, train_end_date, eval_start_date, eval_end_date, top_n
+        )
         self.retrievers = retrievers
 
-    def fit(
-        self, df: cudf.DataFrame, start_date: pd.Timestamp, end_date: pd.Timestamp
-    ) -> None:
+    def fit(self, df: cudf.DataFrame) -> None:
 
         for retriever in self.retrievers:
-            retriever.fit(df, start_date, end_date)
+            retriever.fit(df)
 
     def search(self, users: List[str]):
         self.candidate_items = {user: [] for user in users}
@@ -289,4 +276,4 @@ class ConcatRetriever(Retriever):
                 items = self.candidate_items[user]
                 items += retriever.candidate_items[user]
                 items = order_immutable_deduplication(items)
-                self.candidate_items[user] = items
+                self.candidate_items[user] = items[: self.top_n]
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
-from typing import List
+from typing import List, Tuple
+
+import cudf
+import pandas as pd
 
 
 def order_immutable_deduplication(items: List[str]) -> List[str]:
@@ -15,5 +18,52 @@ def order_immutable_deduplication(items: List[str]) -> List[str]:
     return sorted(set(items), key=items.index)
 
 
-def flatten_2d_list(items: List[List[str]]) -> List[str]:
-    return sum(items, [])
+def flatten_2d_list(list_2d: List[List[str]]) -> List[str]:
+    """Convert 2d list to 1d.
+
+    Args:
+        list_2d (List[List[str]]): 2d list before conversion.
+
+    Returns:
+        List[str]: 1d list after conversion.
+    """
+    return sum(list_2d, [])
+
+
+def get_data_period(
+    date_th: str, train_period: int, eval_period: int
+) -> Tuple[pd.Timestamp, pd.Timestamp, pd.Timestamp, pd.Timestamp]:
+    """Calculate the first and last days of the train period and evaluation period.
+
+    Args:
+        date_th (str): Boundary between train period and evaluation period.
+        train_period (int): Number of days of train period.
+        eval_period (int, optional): Number of days of evaluation period. Defaults to 7.
+
+    Returns:
+        Tuple[pd.Timestamp, pd.Timestamp, pd.Timestamp, pd.Timestamp]: First and last day of study period and evaluation period.
+    """
+    train_start = pd.to_datetime(date_th) - pd.Timedelta(train_period, "days")
+    train_end = pd.to_datetime(date_th)
+    eval_start = pd.to_datetime(date_th)
+    eval_end = pd.to_datetime(date_th) + pd.Timedelta(eval_period, "days")
+    return train_start, train_end, eval_start, eval_end
+
+
+def period_extraction(
+    df: cudf.DataFrame, start_date: pd.Timestamp, end_date: pd.Timestamp
+) -> cudf.DataFrame:
+    """Extract data for specific period.
+
+    Args:
+        df (cudf.DataFrame): Preprocessed data.
+        start_date (pd.Timestamp): First day of extraction period.
+        end_date (pd.Timestamp): Last day of extraction period.
+
+    Returns:
+        cudf.DataFrame: Data after extraction.
+    """
+    df = df[
+        (start_date < df["time_stamp"]) & (df["time_stamp"] <= end_date)
+    ].reset_index(drop=True)
+    return df
